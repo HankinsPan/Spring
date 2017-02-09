e36f85f10e39c06fa42d97f72f0e41a14957d4c7
HankinsPan <Hankins.Home@outlook.com>
2017-02-09 22:33:28 +0800

    Modified Sign View2.2 #login & register view finished

--- spring/src/main/java/spr/dev/activity/login/SignActivity.java
+++ spring/src/main/java/spr/dev/activity/login/SignActivity.java
@@ -1,6 +1,8 @@
 package spr.dev.activity.login;
 
 import android.animation.Animator;
+import android.animation.ValueAnimator;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.CountDownTimer;
@@ -15,10 +17,12 @@ import android.view.View;
 import android.view.ViewAnimationUtils;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.view.animation.DecelerateInterpolator;
+import android.view.animation.LinearInterpolator;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 
+import spr.dev.MainActivity;
 import spr.dev.R;
 import spr.dev.util.SharedPreferencesUtil;
 import spr.dev.util.ShowToast;
@@ -42,7 +46,6 @@ public class SignActivity extends AppCompatActivity
     private RelativeLayout mRelat;
 
     private Scene mSceneLogging;
-    private Scene mSceneMain;
 
     private AutoCompleteTextView aot_phone;
     private AutoCompleteTextView aot_smsCode;
@@ -62,7 +65,7 @@ public class SignActivity extends AppCompatActivity
         public void onFinish() {
             btn_smsCode.setText("SmsCode");
             btn_smsCode.setClickable(true);
-            btn_smsCode.setBackgroundColor(Color.parseColor("#2D78B1"));
+            btn_smsCode.setBackgroundColor(getResources().getColor(R.color.colorBg));
         }
     };
 
@@ -79,18 +82,82 @@ public class SignActivity extends AppCompatActivity
     }
 
     private void initView() {
-        mRelat = (RelativeLayout) findViewById(R.id.relat_content);
+        mRelat = (RelativeLayout) findViewById(R.id.rlt_bg);
         mDuration = getResources().getInteger(R.integer.duration);
 
         mSceneLogging = Scene.getSceneForLayout(mRelat, R.layout.scene_logging, this);
-        mSceneMain = Scene.getSceneForLayout(mRelat, R.layout.scene_new_main, this);
-
+        LoadAnim();
 
         aot_phone = (AutoCompleteTextView) findViewById(R.id.aot_phone);
         aot_smsCode = (AutoCompleteTextView) findViewById(R.id.aot_smsCode);
         btn_smsCode = (Button) findViewById(R.id.btn_smsCode);
         signViewBtn = (SignView) findViewById(R.id.signView_btn);
 
+    }
+
+    private void LoadAnim() {
+        mSceneLogging.setEnterAction(new Runnable() {
+            @Override
+            public void run() {
+                final SignView signViewBtn = (SignView)
+                        mRelat.findViewById(R.id.signView_btn);
+
+                signViewBtn.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        signViewBtn.setStatus(SignView.STATUS_LOGGING);
+                    }
+                }, mDuration);
+
+                signViewBtn.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        signViewBtn.setStatus(SignView.STATUS_LOGIN_SUCCESS);
+                    }
+                }, 3000);
+
+
+                final RelativeLayout mRelat_live = (RelativeLayout)
+                        mRelat.findViewById(R.id.rlt_logging);
+
+                mRelat_live.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        ValueAnimator animator = ValueAnimator.ofFloat(0, -mRelat_live.getHeight());
+                        animator.setDuration(mDuration);
+                        animator.setInterpolator(new LinearInterpolator());
+                        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                            @Override
+                            public void onAnimationUpdate(ValueAnimator animation) {
+                                float mHeight = (float) animation.getAnimatedValue();
+                                mRelat_live.setY(mHeight);
+                            }
+                        });
+                        animator.start();
+                    }
+                }, 4500);
+
+                mRelat_live.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Intent intent = new Intent(SignActivity.this, MainActivity.class);
+                        startActivity(intent);
+                    }
+                },5000);
+
+                mRelat_live.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Finished();
+                    }
+                },5200);
+            }
+        });
+
+    }
+
+    private void Finished() {
+        this.finish();
     }
 
     private void addListener() {
@@ -108,6 +175,11 @@ public class SignActivity extends AppCompatActivity
                         && Tools.validatePhone(uPhone)
                         && uPhone.equals(i_Phone)) {
                     SendSmsCode();
+                    btn_smsCode.setText("59"+" s");
+                    timer.start();
+                    btn_smsCode.setClickable(false);
+                    btn_smsCode.setBackgroundColor(getResources().getColor(R.color.theme_blue_two));
+
                     ShowToast.ColorToast(SignActivity.this, "SmsCode is Send", 1200);
                 } else {
                     ShowToast.ColorToast(SignActivity.this, "phone number is error", 1200);
@@ -136,8 +208,6 @@ public class SignActivity extends AppCompatActivity
 
             default:
                 break;
-
-
         }
 
     }
@@ -154,6 +224,9 @@ public class SignActivity extends AppCompatActivity
         int x = location[0];
         int y = location[1];
 
+        Log.e(TAG, "-- x -- >" + x);
+        Log.e(TAG, "-- y -- >" + y);
+
         Animator anim = ViewAnimationUtils.createCircularReveal(mRelat,
                 x + signViewBtn.getWidth() / 2,
                 y - signViewBtn.getHeight() / 2,
@@ -166,22 +239,23 @@ public class SignActivity extends AppCompatActivity
         anim.addListener(new Animator.AnimatorListener() {
             @Override
             public void onAnimationStart(Animator animation) {
-
+                Log.e(TAG, "-- onAnimationStart --");
             }
 
             @Override
             public void onAnimationEnd(Animator animation) {
-
+                mRelat.setBackgroundColor(Color.TRANSPARENT);
+                Log.e(TAG, "-- onAnimationEnd --");
             }
 
             @Override
             public void onAnimationCancel(Animator animation) {
-
+                Log.e(TAG, "-- onAnimationCancel --");
             }
 
             @Override
             public void onAnimationRepeat(Animator animation) {
-
+                Log.e(TAG, "-- onAnimationRepeat --");
             }
         });
 
